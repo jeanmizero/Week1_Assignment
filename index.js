3b065a59de1e3f967707bd44c1bb1b3b550ba703
Jean <mizero2@gmail.com>
2020-11-17 12:59:47 -0700

    week1 assignment

---
+++ index.js
@@ -0,0 +1,54 @@
+// Item price
+var priceItem1 = 5;
+var priceItem2 = 5.1;
+// Print out the values
+console.log(priceItem1);
+console.log(priceItem2);
+//Amount of money in wallet
+var moneyInWallet1 = 100;
+var moneyInWallet2 = 80.5;
+// Print out the values
+console.log(moneyInWallet1);
+console.log(moneyInWallet2);
+//Number of friends
+var numberOfFriends1 = 5;
+var numberOfFriends2 = 10;
+// Print out the values
+console.log(numberOfFriends1);
+console.log(numberOfFriends2);
+// Age in years(as whole number)
+var myAge1 = 28;
+var myAge2 = 34;
+// Print out the values
+console.log(myAge1);
+console.log(myAge2);
+//First name
+var firstName1 = "Jean";
+var firstName2 = "Joe";
+// Print out the values
+console.log(firstName1);
+console.log(firstName2);
+// Last name
+var lastName1 = "Mizero";
+var lastName2 = "Doe";
+// Print out the values
+console.log(lastName1);
+console.log(lastName2);
+// Middle Initial
+var middleInitial1 = "B";
+var middleInitial2 = "S";
+// Print out the values
+console.log(middleInitial1);
+console.log(middleInitial2);
+// New balance
+let newBalance = (moneyInWallet1 - moneyInWallet2);
+// Print out the values
+console.log(newBalance);
+// New number of friends
+let newFiends = (numberOfFriends1 + numberOfFriends2);
+// Print out the values
+console.log(newFiends);
+//Full name
+let fullName = (firstName1 + " " + middleInitial1 + " " + " " + lastName1 );
+// Print out the values
+console.log("My full name is: " + fullName);
